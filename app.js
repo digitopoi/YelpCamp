e78ccc320ee44306284215e07d54877831afbce3
Hugh Bowman <hubo85@gmail.com>
2017-07-10 02:10:08 -0500

    Add test landing page

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var app = express();
 
+app.get("/",
+    function(req, res) {
+        res.send("This will be the landing page... soon!");
+    });
 
-app.listen(3000, function() {
-    console.log("The YelpCamp server has started!")
-});
+
+app.listen(3000,
+    function () {
+        console.log("The YelpCamp server has started!")
+    });
