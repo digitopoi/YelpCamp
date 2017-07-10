7a1732cbc53bd19da8ae76de4dba29abb072f406
Hugh Bowman <hubo85@gmail.com>
2017-07-10 03:22:42 -0500

    Add "add campground" functionality

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //  TEMP CAMPGROUND ARRAY - [{name: <name>, image: <url>}]
 var campgrounds = [
     { name: "Salmon Creek", image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg" },
     { name: "Granite Hill", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg" },
-    { name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg" }
 
 ];
 
@@ -20,9 +23,24 @@ app.get("/campgrounds",
         res.render("campgrounds", {campgrounds: campgrounds});
     });
 
+app.post("/campgrounds",
+    function (req, res) {
+        //  get data from form and add to campgrounds array
+        var name = req.body.name;
+        var image = req.body.image;
+        var newCampground = { name: name, image: image };
+        campgrounds.push(newCampground);
+        //  redirect to campgrounds page
+        res.redirect("/campgrounds");
+    });
+
+app.get("/campgrounds/new",
+    function(req, res) {
+        res.render("new.ejs");
+    });
 
 //  SERVER
 app.listen(3000,
     function () {
-        console.log("The YelpCamp server has started!")
+        console.log("The YelpCamp server has started!");
     });
