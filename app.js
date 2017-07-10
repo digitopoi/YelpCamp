08f11cc33b4681e4856faaa358041da0cbf996c5
Hugh Bowman <hubo85@gmail.com>
2017-07-10 02:14:19 -0500

    Add landing view

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/",
     function(req, res) {
-        res.send("This will be the landing page... soon!");
+        res.render("landing");
     });
 
 
