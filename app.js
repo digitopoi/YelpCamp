38c4a5179512692d6f385856de9b03c6607187cd
digitopoi <hubo85@gmail.com>
2017-07-18 20:14:18 -0500

    Add log in check to comments page

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     //  lookup campground using id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
