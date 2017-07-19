385606f1ca3988af25d55a98cb0ab86856651cc0
digitopoi <hubo85@gmail.com>
2017-07-19 12:31:02 -0500

    Save username+id to newly created campground and display with campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,8 +19,12 @@ router.post("/", isLoggedIn, function (req, res) {
     //  get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var desc = req.body.description;     
-    var newCampground = { name: name, image: image, description: desc };
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     //  Create new campground and save to database
     Campground.create(newCampground, function(err, newlyCreated) {
         if(err){
