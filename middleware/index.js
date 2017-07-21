73d0cf0ac6e23253df77f6ff11ce5c15d1476328
digitopoi <hubo85@gmail.com>
2017-07-21 18:41:25 -0500

    Refactor middleware

---
+++ middleware/index.js
@@ -0,0 +1,54 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    //  Is user logged in?
+    if(req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if(err) {
+                res.redirect("back");
+            } else {
+                //  Does user own campground?
+                if(foundCampground.author.id.equals(req.user._id)) {      // foundCampground.author.id is an object 
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+    //  Is user logged in?
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect("back");
+            } else {
+                //  Does user own comment?
+                if(foundComment.author.id.equals(req.user._id)) {      // foundComment.author.id is an object 
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+} 
+
+middlewareObj.isLoggedIn = function (req, res, next){
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+module.exports = middlewareObj;
--- routes/comments.js
+++ routes/comments.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router({mergeParams: true});
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
+var middleware = require("../middleware");
 
 //  New comments route
-router.get("/new", isLoggedIn, function(req, res) {
+router.get("/new", middleware.isLoggedIn, function(req, res) {
     //  find campground by id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -16,7 +17,7 @@ router.get("/new", isLoggedIn, function(req, res) {
 });
 
 //  Comments Create route
-router.post("/", isLoggedIn, function(req, res) {
+router.post("/", middleware.isLoggedIn, function(req, res) {
     //  lookup campground using id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -45,7 +46,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 //  Edit comments route
-router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
             res.redirect("back");
@@ -56,7 +57,7 @@ router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
 });
 
 //  Update comments route
-router.put("/:comment_id", checkCommentOwnership, function(req, res) {
+router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if(err) {
             console.log(err);
@@ -68,7 +69,7 @@ router.put("/:comment_id", checkCommentOwnership, function(req, res) {
 });
 
 //  Destroy comments route
-router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if(err) {
             res.redirect("back");
@@ -77,37 +78,5 @@ router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
         }
     });
 });
-
-
-//  ============================================
-//      MIDDLEWARE - check if logged in
-//  ============================================
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
-
-function checkCommentOwnership(req, res, next) {
-    //  Is user logged in?
-    if(req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, function(err, foundComment) {
-            if(err) {
-                res.redirect("back");
-            } else {
-                //  Does user own comment?
-                if(foundComment.author.id.equals(req.user._id)) {      // foundComment.author.id is an object 
-                    next();
-                } else {
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        res.redirect("back");
-    }
-} 
 
 module.exports = router;
